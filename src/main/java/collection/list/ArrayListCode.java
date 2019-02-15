f419352cd698b63b0a6c59666cc6d0b6272e7a06
wsz <wsz>
2019-02-15 14:45:03 +0800

    测试list效率，ArrayList源码分析

--- src/main/java/collection/list/ArrayListCode.java
+++ src/main/java/collection/list/ArrayListCode.java
@@ -2,6 +2,7 @@ package collection.list;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * ArrayList源码分析
@@ -15,8 +16,22 @@ public class ArrayListCode {
 	 * 1.Arrays.copyOf(int[] original,int newLength)
 	 * 2.System.arraycopy(Object src,int srcPos,Object dest,int destPos,int length)
 	 */
-	ArrayList<Object> list = new ArrayList<>();
 	
+/**
+ * 1.底层为数组Object[] elementData，无初始容量构造参数时为EMPTY_ELEMENTDATA，初始容量构参为0时DEFAULTCAPACITY_EMPTY_ELEMENTDATA
+ * 2.无参构造初始化时size=0，初次add()/addAll(Collection c)会判断elementData是否为DEFAULTCAPACITY_EMPTY_ELEMENTDATA然后取默认的容量DEFAULT_CAPACITY=10或者自动更新size+=c.length
+ * 3.initialCapacity初始容量有参构造，elementData长度为initialCapacity
+ * 4.集合Collection构造参数初始化时，转为Object[]，判断集合.length>0，直接使用Arrays.copyOf复制到elementData
+ * 5.扩容1.5倍，int newCapacity = oldCapacity + (oldCapacity >> 1);
+ */
+	
+/**
+ * add：size+1，判断是否需要扩容，(不)扩容后直接新增到size+1下标位
+ * get
+ * remove
+ * set
+ */
+	List<Object> list = new ArrayList<>();
 	public static void main(String[] args) {
 		int[] elementData = {12,254,547,455};
  		elementData = Arrays.copyOf(elementData, 6);
--- src/main/java/collection/list/ListEfficiency.java
+++ src/main/java/collection/list/ListEfficiency.java
@@ -8,9 +8,17 @@ import java.util.Random;
 import org.junit.Test;
 
 /**
- * 比较ArrayList与LinkedList的增删效率
- * 1.ArrayList底层为数组实现，需要对数组进行拷贝复制；增删较低慢，查询较快
- * 2.LinkedList底层为双向链表实现；增删较快，查询较慢
+ * 比较ArrayList与LinkedList的效率
+ * 1.ArrayList底层为数组实现，需要对数组进行拷贝复制、数据移位；增删较低慢，查询较快；
+ * 		get(index)-O(1)
+ * 		add(E)-O(1)
+ * 		add(index,E)-O(n)
+ * 		remove()-O(n)
+ * 2.LinkedList底层为双向链表实现；增删较快，查询较慢；
+ * 		get()-O(n)，涉及到链表遍历
+ * 		add(E)-O(1)，直接添加到链表结尾
+ * 		add(index,E)-O(n)，涉及到链表遍历与指针指向修改
+ * 		remove()-O(1)，针对确定的节点为O(1)。但在源码中需要先遍历获取到当前节点则为O(n)
  * @author wsz
  * @date 2019年2月13日
  */
@@ -28,15 +36,20 @@ public class ListEfficiency {
 /*
  *    随机插入消耗时间，单位微秒
  		   ArrayList    		 LinkedList
-  10W    12-7-5-18-8		    13-6-5-9-5
-  50W    19-17-18-19-18         66-77-66-105-64
-  100W   62-60-64-62-70         109-102-107-105-107
-  500W   1375-1410-1345-1396    3136-3120-3142-3231
+  1W     12-11-9-10-11	      131-155-145-158-190
+  2W	 19-21-24-21-22		  615-731-712-622-648
+  5W     90-86-87             8208-7789-7668
  */	
+	
+	@Test
+	public void testDemo() {
+		randomGet(new ArrayList<>(10));
+		randomGet(new LinkedList<>());
+	}
 	
 	int dataSize = 5000000;
 	//顺序插入尾部
-	public void time(List<Object> list) {
+	public void orderInsert(List<Object> list) {
 		long arrayS = System.currentTimeMillis();
 		for(int i=0; i<dataSize;i++) {
 			list.add(i);
@@ -45,7 +58,7 @@ public class ListEfficiency {
 		System.out.println(arrayE - arrayS);
 	}
 	
-	//随机插入
+	//随机插入：
 	public void randomInsert(List<Object> list) {
 		long arrayS = System.currentTimeMillis();
 		Random random = new Random();
@@ -57,9 +70,16 @@ public class ListEfficiency {
 		System.out.println(arrayE - arrayS);
 	}
 	
-	@Test
-	public void addAndDelete() {
-		randomInsert(new ArrayList<>(10));
-		randomInsert(new LinkedList<>());
+	//随机获取：测试结果差异不大
+	public void randomGet(List<Object> list) {
+		for(int i=1; i <= dataSize;i++) {
+			list.add(i);
+		}
+		Random random = new Random();
+		int r = random.nextInt(dataSize);
+		long arrayS = System.currentTimeMillis();
+		System.out.print("random:"+r+" data:"+list.get(r)+"  time:");
+		long arrayE = System.currentTimeMillis();
+		System.out.println(arrayE - arrayS);
 	}
 }
