8481a701b3467b905697674819020ead941c75a6
wsz <wsz>
2019-02-26 13:46:01 +0800

    LRU-K算法

--- src/main/java/algorithm/LRUCache.java
+++ src/main/java/algorithm/LRUCache.java
@@ -1,76 +1,132 @@
 package algorithm;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 /**
- * LRU模拟算法
+ * LRU-K模拟算法
  * Least recently used，最近最少使用
  * @author wsz
  * @date 2019年2月21日
  */
-public class LRUCache {
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class LRUCache<K,V> {
 
-	static final int DEFAULT_CAPACITY = 2^5;
+	static final int DEFAULT_RATE = 1;
+	static final int MAXIMUN_RATE = 1 << 5;
+	
+	static final int DEFAULT_CAPACITY = 16;
 	static final int MAXIMUM_CAPACITY = 1 << 30;
 	
+	@SuppressWarnings("unused")
+    private static class Item<K, V, N> {
+        K key;
+        V value;
+        N rate;
+        
+        Item(K key, V value, N rate) {
+            this.key = key;
+            this.value = value;
+            this.rate = rate;
+        }
+        
+		public final K getKey()        { return key; }
+        public final V getValue()      { return value; }
+        public final N getRate()      { return rate; }
+        public final void setRate(N rate)   {this.rate = rate;}
+        public final String toString() { return key + "=" + value + "=" + rate; }
+        
+        public final Map castToMap() {
+        	Map map = new HashMap(3);
+        	map.put("key", key);
+        	map.put("value", value);
+        	map.put("rate", rate);
+        	return map;
+        }
+    }
+    
 	/**
 	 * 缓存容量
 	 */
-	int capacity;
+	private int capacity;
+	
+	/**
+	 * 默认频率，访问记录rate次后加入到缓存列表中
+	 */
+	private int rate;
 	
 	/**
 	 * 双向链表LinkedList模拟缓存列表
 	 */
-	LinkedList<Object> keys;
+	private LinkedList cacheList;
 	
 	/**
 	 * 缓存键值对
 	 */
-	Map<Object,Object> map;
+	private Map<K,V> map;
 	
 	public LRUCache() {
-		this(DEFAULT_CAPACITY);
+		this(DEFAULT_CAPACITY, DEFAULT_RATE);
 	}
 
 	public LRUCache(int initialCapacity) {
+		this(initialCapacity, DEFAULT_RATE);
+	}
+	
+	public LRUCache(int initialCapacity, int initRate) {
 		if(initialCapacity < 0)
 			throw new IllegalArgumentException("Illegal initial capacity: " + initialCapacity);
 		if(initialCapacity > MAXIMUM_CAPACITY) {
 			initialCapacity = MAXIMUM_CAPACITY;
 		}
+		if(initRate < 0)
+			throw new IllegalArgumentException("Illegal initial rate: " + initRate);
+		if(initRate > MAXIMUN_RATE) {
+			initRate = MAXIMUN_RATE;
+		}
+		
 		this.capacity = initialCapacity;
+		this.rate = initRate;
 		resize();
 	}
 	
-	public Object get(Object key) {
-		Object object = map.get(key);
-		if(object != null) {
-			keys.remove(key);
-			keys.addFirst(key);
-			return object;
-		}else {
-			return -1;
+	public V get(K key) {
+		Item item = (Item) map.get(key);
+		if(item != null) {
+			int oldRate = (int) item.getRate();
+			int newRate = oldRate + 1;
+			item.setRate(newRate);
+			map.put(key, (V) item);
+			
+			if(newRate >= rate) {
+				//LinkedList非线程安全，直接加锁同步
+				synchronized(cacheList) {
+					if(cacheList.contains(key))
+						cacheList.remove(key);
+					cacheList.addFirst(key);
+					if(size() > capacity) {
+						cacheList.removeLast();;
+					}
+				}
+			}
+			return (V) item.getValue();
 		}
+		return null;
 	}
 	
-	public Object put(Object key, Object value) {
-		Object object = map.get(key);
-		if(object != null && keys.contains(key)) {
-			keys.remove(key);
-		}
-		map.put(key, value);
-		keys.addFirst(key);
-		if(size() > capacity) {
-			Object del = keys.removeLast();;
-			map.remove(del);
-		}
-		return object;
+	public V put(K key, V value) {
+		V item = (V) new Item(key, value, 0);
+		map.put(key, item);
+		return item;
 	}
 	
 	public void resize() {
-		if(keys == null) {
-			keys = new LinkedList<>();
+		if(cacheList == null) {
+			cacheList = new LinkedList<>();
 		}
 		if(map == null) {
 			map = new HashMap<>(capacity);
@@ -78,6 +134,24 @@ public class LRUCache {
 	}
 	
 	public int size() {
-		return keys.size();
+		return cacheList == null ? 0 : cacheList.size();
+	}
+	
+	public List getItems() {
+		List re = new ArrayList();
+		Set<Entry<K, V>> entrySet = map.entrySet();
+		for (Entry<K, V> entry : entrySet) {
+			Item item = (Item) entry.getValue();
+			re.add(item.castToMap());
+		}
+		return re;
+	}
+	public List getCacheList() {
+		return cacheList;
+	}
+	
+	@Override
+	public String toString() {
+		return "LRUCache [capacity=" + capacity + ", rate=" + rate + ", cacheList=" + cacheList + ", map=" + map + "]";
 	}
 }
--- src/main/java/algorithm/LRUTest.java
+++ src/main/java/algorithm/LRUTest.java
@@ -1,27 +1,91 @@
 package algorithm;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
+import annotation.Desc;
+/**
+ * LRU-K 测试
+ * @author wsz
+ * @date 2019年2月21日
+ */
 public class LRUTest {
 
 	@Test
+	@Desc("默认LRU-1")
 	public void lruTest() {
-		LRUCache cache = new LRUCache(5);
+		LRUCache<Object, Object> cache = new LRUCache<Object, Object>(5);
 		cache.put("a", "1");
 		cache.put("b", "2");
 		cache.put("c", "3");
-		System.out.println(cache.keys);
+		System.out.println(cache.getCacheList());
 		cache.get("a");
-		System.out.println(cache.keys);
+		System.out.println(cache.getCacheList());
 		cache.put("d", "4");
-		System.out.println(cache.keys);
+		System.out.println(cache.getCacheList());
 		cache.put("e", "5");
-		System.out.println(cache.keys);
+		System.out.println(cache.getCacheList());
 		System.out.println(cache.get("f"));
 		cache.put("f", "6");
-		System.out.println(cache.keys);
+		System.out.println(cache.getCacheList());
 		cache.put("f", "7");
-		System.out.println(cache.keys);
+		System.out.println(cache.getCacheList());
 		System.out.println(cache.get("f"));
+	}
+	
+	@Test
+	@Desc("LRU-K测试")
+	public void kLRUTest() throws InterruptedException {
+		
+		LRUCache<Integer, Integer> synCache = new LRUCache<Integer, Integer>(10, 5);
+		Random random = new Random();
+		int range = 20;
+		
+		for(int i=0; i < range; i++) {
+			synCache.put(i, i);
+		}
+		for(int k=0; k < 80; k++) {
+			synCache.get(random.nextInt(range));
+		}
+		
+		for (Object obj : synCache.getItems()) {
+			System.out.println(obj);
+		}
+		System.out.println(synCache.getCacheList());
+	}
+	
+	@Test
+	@Desc("测试并发情况")
+	public void synTest() throws Exception {
+		for(int j=0; j<20; j++) {
+			cache.put(j, j);
+		}
+		//array有界阻塞队列
+		BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(queueSize);
+		//100个任务线程
+		ThreadPoolExecutor service = new ThreadPoolExecutor(queueSize/2, taskSize, 10, TimeUnit.SECONDS, queue);
+		for(int i=0; i< taskSize; i++) {
+			service.execute(new SynTask());
+		}
+		service.shutdown();
+		service.awaitTermination(10, TimeUnit.SECONDS);
+		System.out.println(cache.getItems());
+		System.out.println(cache.getCacheList());
+	}
+	LRUCache<Integer, Integer> cache = new LRUCache<Integer, Integer>(20,5);
+	Random randon = new Random();
+	int queueSize = 20;
+	int taskSize = 100;
+	
+	class SynTask implements Runnable{
+		@Override
+		public void run() {
+			cache.get(randon.nextInt(20));
+		}
 	}
 }
